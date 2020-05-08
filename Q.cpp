b1b713f115ccb50f57f09e06d2119f99bb857e15
Trần Quang Tiến <tqtnk2000@gmail.com>
2020-05-08 21:09:13 +0700

    AND, OR, XOR, NOT, >>, <<, +, -

--- Q.cpp
+++ Q.cpp
@@ -47,3 +47,135 @@ void QInt::PrintDec(){
 	}
 	cout << kq;
 }
+
+int& QInt::operator[](int ind)
+{
+    return data[ind];
+}
+
+QInt& QInt::operator=(QInt a)
+{
+    for (int i=0;i<4;i++)
+        (*this)[i]=a[i];
+    return *this;
+}
+
+QInt QInt::operator&(QInt a)
+{
+    QInt tmp=*this;
+    for (int i=0;i<4;i++)
+    {
+        tmp[i]=tmp[i]&a[i];
+    }
+    return tmp;
+}
+
+QInt QInt::operator|(QInt a)
+{
+      QInt tmp=*this;
+       for (int i=0;i<4;i++)
+       {
+           tmp[i]=tmp[i]|a[i];
+       }
+       return tmp;
+}
+
+QInt QInt::operator^(QInt a)
+{
+      QInt tmp=*this;
+       for (int i=0;i<4;i++)
+       {
+           tmp[i]=tmp[i]^a[i];
+       }
+       return tmp;
+}
+
+QInt QInt::operator~()
+{
+      QInt tmp=*this;
+       for (int i=0;i<4;i++)
+       {
+           tmp[i]=~tmp[i];
+       }
+       return tmp;
+}
+
+QInt QInt::operator>>(int a)
+{
+    QInt tmp=*this;
+    if (a<0) return tmp<<(-a);
+    if (a<=32)
+    {
+        for (int i=3;i>0;i--)
+        {
+            tmp[i]=(tmp[i]>>a)|(tmp[i-1]<<(32-a));
+        }
+        tmp[0]=tmp[0]>>a;
+    }
+    else
+    {
+        for (int i=3;i>0;i--)
+              {
+                  tmp[i]=tmp[i-1];
+              }
+        tmp[0]=tmp[0] < 0 ? -1 : 0;
+        tmp=tmp>>(a-32);
+    }
+    return tmp;
+}
+
+QInt QInt::operator<<(int a)
+{
+    QInt tmp=*this;
+    if (a<0) return tmp>>(-a);
+    if (a<=32)
+    {
+        for (int i=0;i<4;i++)
+        {
+            tmp[i]=(tmp[i]<<a)|(tmp[i+1]>>(32-a));
+        }
+    }
+    else
+    {
+        for (int i=0;i<4;i++)
+              {
+                  tmp[i]=tmp[i+1];
+              }
+        tmp[3]=0;
+        tmp=tmp<<(a-32);
+    }
+    return tmp;
+}
+
+QInt QInt::operator+(QInt a)
+{
+    QInt tmp1=*this;
+    QInt zero;
+    QInt carry;
+    while (a!=zero)
+    {
+        carry= tmp1 & a;
+        tmp1=tmp1^a;
+        carry=carry<<1;
+        a = carry;
+    }
+    return tmp1;
+}
+
+
+QInt QInt::operator-(QInt a)
+{
+    
+    QInt bu(1);
+    a= ~a + bu;
+    
+    return *this+a;
+}
+
+bool QInt::operator!=(QInt a)
+{
+    QInt tmp=*this;
+    for (int i=0;i<4;i++)
+        if (tmp[i]!=a[i]) return true;
+    return false;
+}
--- Q.h
+++ Q.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include <iostream>
 using namespace std;
 class QInt{
@@ -8,10 +8,40 @@ public:
 		for (int i = 0; i < 4; i++)
 			data[i] = 0;
 	}
+    QInt (int n)
+    {
+      for (int i = 0; i < 3; i++)
+        data[i] = 0;
+        data[3]=n;
+    }
 	friend ostream& operator << (ostream&os, QInt p);
 	void SetBitFromBin(bool* bin);
 	void Input();
 	void PrintDec();	//in chuỗi thập phân
 	void PrintBin();	//in chuỗi nhị phân
 	~QInt(){}
+    
+    QInt& operator=(QInt a);
+      
+      int& operator[](int ind);
+      
+      QInt operator&(QInt a);
+      QInt operator|(QInt a);
+      QInt operator^(QInt a);
+      QInt operator~();
+      
+      QInt operator+(QInt a);
+      QInt operator-(QInt a);
+      QInt operator*(QInt a);
+      QInt operator/(QInt a);
+      
+      QInt operator>>(int n);
+      QInt operator<<(int n);
+      
+      bool operator<(QInt a);
+      bool operator<=(QInt a);
+      bool operator>(QInt a);
+      bool operator>=(QInt a);
+      bool operator!=(QInt a);
+      bool operator==(QInt a);
 };
